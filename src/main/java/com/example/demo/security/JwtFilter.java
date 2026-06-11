a92a86244f6cb502aff5dae81abfdbb5b3460f83
Icesuza <hengliza81@gmail.com>
2026-06-11 22:13:30 +0700

    chore: modified jwt filter

--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -28,6 +28,13 @@ public class JwtFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
+        String requestURI = request.getRequestURI();
+
+        if (requestURI.contains("/v3/api-docs") || requestURI.contains("/swagger-ui")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
